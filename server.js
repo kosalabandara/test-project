c9de31d6d4185a7a6fd34a50f43da35a50161ac8
kosalabandara <kosalabandara94@gmail.com>
2017-11-20 12:02:32 -0800

    new update

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log('May Node be with you')
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+
+app.get('/', (req, res) => {
+  res.sendfile(__dirname + '/index.html')
+})
