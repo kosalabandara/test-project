3aba9b8d774365006fe8ad5f781d596534564738
kosalabandara <kosalabandara94@gmail.com>
2017-11-20 20:31:48 -0800

    Try to read and create

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-console.log('May Node be with you')
 
 const express = require('express');
 const app = express();
@@ -10,4 +9,40 @@ app.listen(3000, function() {
 
 app.get('/', (req, res) => {
   res.sendfile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+})
+
+
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('link-to-mongodb', (err, database) => {
+ 
+})
+
+db.collection('quotes').find().toArray(function(err, results) {
+  console.log(results)
+  Great! You now see an array of quotes (I only have one right now). We’ve completed the first part – getting data from MongoLab. The next part is to generate a HTML that contains all our quotes.
+})
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
 })
